276aa17bfd19a9de3de46647a8f285050429439f
Rohankale0604 <146310283+Rohankale0604@users.noreply.github.com>
2024-03-26 14:28:06 +0530

    app.py

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ if check == 'Team':
 if check == 'ML - Modeling':
     
     # Load the pre-trained model, TF-IDF Vectorizer, and LabelEncoder
-    model = load(open(r'', 'rb'))
+    model = load(open(r'https://github.com/Rohankale0604/NLP-Project/blob/main/model.pkl', 'rb'))
     vectorizer = load(open('vectorizer.pkl', 'rb'))
     encoder = load(open(r'https://github.com/Rohankale0604/NLP-Project/blob/main/encoder.pkl', 'rb'))
     
